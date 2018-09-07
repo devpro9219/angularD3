9228eeb267f63388128bcbcdb5a19e812489f48e
GPTechSolution <braceliu987@gmail.com>
2018-09-07 23:31:41 +0800

    Version 1.0

---
+++ deployaws.sh
@@ -0,0 +1,19 @@
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/home/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/home/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/login/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/login/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/map/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/map/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/organization/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/organization/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/execution/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/execution/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/performance/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/performance/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/performance/store/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/performance/store/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/performance/store/services/*.html
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/performance/store/services/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/strategy/store/services/*.ts
+sed -i -- 's/127.0.0.1/10.10.1.185/g' ./src/app/strategy/store/services/*.html
+
